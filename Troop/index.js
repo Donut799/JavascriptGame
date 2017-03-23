8b14a711b12c9167489da7b1ba7d8a8ecbbfd0e0
Brendan <donuts799@gmail.com>
2017-03-23 19:58:49 -0400

    minor changes to troop module

--- Troop/index.js
+++ Troop/index.js
@@ -7,7 +7,8 @@ Number.isInteger = Number.isInteger || function(value) {
     Math.floor(value) === value;
 };
 
-function Troop(typeName, AI, defense, attack, ID, weapon, world, health) {//PUAPDO
+function Troop(isGround, typeName, AI, defense, attack, ID, weapon, world, health) {//PUAPDO
+	this.isGround = isGround;
 	this.ai = AI;
 	this.typeName = typeName;
 	this.health = health;//represents max health if i introduce healing
@@ -20,11 +21,12 @@ function Troop(typeName, AI, defense, attack, ID, weapon, world, health) {//PUAP
 	this.world = world;
 }
 //NOTE: the world will be asked to do the attacking, moving, and manage things like this
+//TODO Figure out all necessary actions.
 Troop.prototype.defend = function(isActive, direction){//direction is int 1-360
 	if(isActive){this.shieldDirection = direction;}
 	else{this.shieldDirection = null;}
 }
-Troop.prototype.attack = function(direction){
+Troop.prototype.attack = function(direction){//direction is int 1-360
 	this.world.attack(this.id,direction,weapon);
 }
 Troop.prototype.attackAt = function(position){
@@ -32,39 +34,25 @@ Troop.prototype.attackAt = function(position){
 }
 Troop.prototype.safeMove = function(xTranslation, yTranslation){
 	if (Number.isInteger(xTranslation) && Number.isInteger(yTranslation)) {
-	//TODO check if move is valid
-	this.move(xTranslation, yTranslation)
-	if (move_Callback)
-		this.move_Callback(xTranslation, yTranslation);
+		this.move(xTranslation, yTranslation)
 	} else {
 		throw new Error("Error. safeMove was not given integers as parameters");
 	}
 }
-Troop.prototype.takeTurn = function() {
+Troop.prototype.takeTurn = function(userInput) {
 	var blank = {};
-	blank.ai = this.ai;//Make sure this points to a blank object,and not a useful one.
-	blank.ai()
-  //TODO
+	blank.ai = this.ai;//Make sure "this" points to a blank object, and not a useful one.
+	//TODO WORK ON FUNCTION CONSTRUCTOR FIRST!!!
+	//blank.ai(this.clone(),)
+    //TODO
 }
-Troop.prototype.clone = function(){
+Troop.prototype.clone = function(world){//World is not required used if you don't want it to clone the world (avoid circular references)
   var clonedTroop = new Troop()
   clonedTroop.typeName = this.typeName;
   clonedTroop.ai = this.ai;
   clonedTroop.position = (this.position.clone || function(value) {
     return value
-  })(this.position);
-  clonedTroop.healthLeft = (this.healthLeft.clone || function(value) {
-    return value
-  })(this.healthLeft);
-  clonedTroop.defenseStrength = (this.defenseStrength.clone || function(value) {
-    return value
-  })(this.defenseStrength);
-  clonedTroop.attack = (this.attack.clone || function(value) {
-    return value
-  })(this.attack);
-  clonedTroop.id = (this.id.clone || function(value) {
-    return value
-  })(this.id);
+  })(this.position);//just in case i decide to remove position cloning, or haven't made it yet etc.
   //TODO
 }
 Troop.prototype.toString = function(){
