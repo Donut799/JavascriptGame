f183c343cb903a26b3e69ae7aafc542313f6f255
Brendan <donuts799@gmail.com>
2017-03-23 20:33:48 -0400

    add TwoDArray module

--- TwoDArray/index.js
+++ TwoDArray/index.js
@@ -1,24 +1,47 @@
-function Position(xPosition, yPosition) {
-  this.x = xPosition;
-  this.y = yPosition;
+function TwoDArray(xLength, yLength) {
+  this.xLength = xLength;
+  this.yLength = yLength;
+  for (var i = 0; i < this.xLength; i++) {
+    var y = [];
+    y.length = this.yLength;
+    for (var index = 0; index < y.length; index++) {
+      y[index] = "";
+    }
+    this[i] = y;
+  }
 }
-Position.prototype.clone = function() {
-  return new Position(this.x, this.y)
+TwoDArray.prototype = []
+TwoDArray.prototype.constructor = TwoDArray
+TwoDArray.prototype.fill = function(fillObject) {
+  for (var x = 0; x < this.xLength; x++) {
+    for (var y = 0; y < this.yLength; y++) {
+      //if the object supports it this will clone the object otherwise it will do a reference copy (irrelevant for primitives)
+      this[x][y] = (fillObject.clone || function(value) {
+        return value
+      })(fillObject);
+    }
+  }
 }
-Position.prototype.move = function(xTranslation, yTranslation, move_CallBack) {
-  this.x += xTranslation;
-  this.y += yTranslation;
-  if (move_CallBack)
-    move_CallBack();
+TwoDArray.prototype.clone = function() {//maybe create ability to have exclusions. . .
+  var cloned = new TwoDArray(this.xLength, this.yLength)
+  for (var x = 0; x < this.xLength; x++) {
+    for (var y = 0; y < this.yLength; y++) {
+      cloned[x][y] = (this[x][y].clone || function(value) {
+        return value
+      })(this[x][y]);
+    }
+  }
+  return cloned
 }
-Position.prototype.moveTo = function(xPosition, yPosition, moveTo_CallBack) {
-  this.x = xPosition;
-  this.y = yPosition;
-  if (moveTo_CallBack)
-    moveTo_CallBack();
-}
-Position.prototype.toString = function() {
-  return this.x + ":" + this.y;
+TwoDArray.prototype.toString = function() {
+  var arrayAsString = "";
+  for (var x = 0; x < this.xLength; x++) {
+    for (var y = this.yLength; y < -1; y--) {
+      arrayAsString += this[x][y].toString();
+    }
+    arrayAsString += "\n";
+  }
+  return arrayAsString;
 }
 
-exports.Position = Position;
+exports.TwoDArray = TwoDArray;
