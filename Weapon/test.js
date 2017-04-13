18f8d0431cf57f9ec468b4f5befc6793559c022a
Brendan <donuts799@gmail.com>
2017-04-12 23:40:59 -0400

    minor changes to weapon deviate function

--- Weapon/test.js
+++ Weapon/test.js
@@ -11,12 +11,12 @@ Weapon.prototype.deviate = function(angle){
 	if(Math.random() * 100 < this.deviationChance){
 		var tempMaxDeviation = this.maxDeviation
 		//if the attack occurs in the same direction as the shield deviation should increase by 2x the proportion of the angle of the attack to the angle of the shield.
-		var angDiff = this.Calc.angDiff(this.angle,this.owner.shieldDirection)
-		if(angDiff < 90){
-			tempMaxDeviation = tempMaxDeviation * (((90 - angDiff) / 90) + 1)
+		if(this.owner.shieldDirection){
+			var angDiff = this.Calc.angDiff(this.angle,this.owner.shieldDirection)
+			if(angDiff < 90){
+				tempMaxDeviation = tempMaxDeviation * (((90 - angDiff) / 90) + 1)
+			}
 		}
-		
-		
 		angle = Calc.anglizer(angle + (tempMaxDeviation * 2 * (Math.random() - .5)));
 	}
 	return angle;
